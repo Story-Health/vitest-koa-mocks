badc0ea93201ec1085534f5db55aa48eaebb2469
Tom Rudick <tmr@storyhealth.ai>
2024-01-11 21:18:24 -0500

    Initial fork

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## 5.1.1
+
+- Initial fork from [@shopify/jest-koa-mocks](https://github.com/Shopify/quilt/tree/main/packages/jest-koa-mocks).
+
+Plan on keeping up with existing versions from the original repository.
---
+++ src/create-mock-cookies/create-mock-cookies.ts
@@ -0,0 +1,37 @@
+import type { Context } from "koa";
+import { vi } from "vitest";
+
+export type Cookies = Context["cookies"];
+
+export interface Dictionary<T> {
+  [key: string]: T;
+}
+
+export interface MockCookies extends Cookies {
+  requestStore: Map<string, string>;
+  responseStore: Map<string, string>;
+}
+
+export default function createMockCookies(
+  cookies = {},
+  secure = true
+): MockCookies {
+  const cookieEntries = Object.keys(cookies).map(
+    (key) => [key, cookies[key]] as [string, string]
+  );
+
+  const requestStore = new Map<string, string>(cookieEntries);
+  const responseStore = new Map<string, string>(cookieEntries);
+
+  return {
+    set: vi.fn((key, value) => {
+      return responseStore.set(key, value);
+    }),
+    get: vi.fn((key) => {
+      return requestStore.get(key);
+    }),
+    requestStore,
+    responseStore,
+    secure,
+  } as any;
+}
